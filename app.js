874c2ee8cf368356034ac35dff0ba3e47a632349
legg2710 <luisemilio2710@gmail.com>
2021-06-14 14:21:02 +0000

    Ejercicio Listo

---
+++ app.js
@@ -0,0 +1,10 @@
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon',];
+
+  let randomain = Math.floor(Math.random() * 2);
+
+  let domain = pronoun[randomain] + adj[randomain] + noun[randomain] + ".com"
+
+  console.log (domain)
+  
